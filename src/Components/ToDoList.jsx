5e097820b1ddb32f084202967fd349b49fa5338d
shahreaz <ashahreaz@gmail.com>
2021-07-11 02:52:35 +0600

    Added local storage and responsiveness

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -7,37 +7,44 @@ export default class ToDoList extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			todos: [
-				{ content: "Eat", id: uuid() },
-				{ content: "Sleep", id: uuid() },
-				{ content: "Code", id: uuid() },
+			todos: JSON.parse(localStorage.getItem("todos")) || [
+				{ content: "Add Todo", id: uuid() },
 			],
 		};
-		localStorage.setItem("todos", this.state.todos);
+		// localStorage.setItem("todos", this.state.todos);
 	}
 
 	addToDo = (todo) => {
-		this.setState((prevState) => {
-			return { todos: [...prevState.todos, todo] };
-		});
+		this.setState(
+			(prevState) => {
+				return { todos: [...prevState.todos, todo] };
+			},
+			() => localStorage.setItem("todos", JSON.stringify(this.state.todos))
+		);
 	};
 
 	deleteToDo = (id) => {
-		this.setState((prevState) => {
-			return { todos: prevState.todos.filter((e) => e.id !== id) };
-		});
+		this.setState(
+			(prevState) => {
+				return { todos: prevState.todos.filter((e) => e.id !== id) };
+			},
+			() => localStorage.setItem("todos", JSON.stringify(this.state.todos))
+		);
 	};
 
 	editToDo = (id, editedItem) => {
-		this.setState((prevState) => {
-			const newTodo = prevState.todos.map((e) => {
-				if (e.id === id) e.content = editedItem;
-				return e;
-			});
+		this.setState(
+			(prevState) => {
+				const newTodo = prevState.todos.map((e) => {
+					if (e.id === id) e.content = editedItem;
+					return e;
+				});
 
-			console.log(newTodo);
-			return { todos: newTodo };
-		});
+				// console.log(newTodo);
+				return { todos: newTodo };
+			},
+			() => localStorage.setItem("todos", JSON.stringify(this.state.todos))
+		);
 	};
 
 	render() {
@@ -52,7 +59,7 @@ export default class ToDoList extends React.Component {
 		return (
 			<div className="ToDoList">
 				<div>
-					<h1 className="ToDoList-title">ToDo App</h1>
+					<h1 className="ToDoList-title">To-Do</h1>
 					<div className="ToDoList-todos">{todos}</div>
 				</div>
 				<TodoForm addToDo={this.addToDo} />
